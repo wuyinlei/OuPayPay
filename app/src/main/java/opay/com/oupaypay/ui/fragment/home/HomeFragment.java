dd2e5d23d7a4f3d420b68b0bc2574c11d577af29
袁冲 <yuanchong@yuanchongdeMacBook-Pro.local>
2017-11-27 18:17:58 +0800

    首页

--- app/src/main/java/opay/com/oupaypay/ui/fragment/home/HomeFragment.java
+++ app/src/main/java/opay/com/oupaypay/ui/fragment/home/HomeFragment.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import opay.com.oupaypay.R;
 import opay.com.oupaypay.app.base.BaseFragment;
@@ -38,7 +41,6 @@ public class HomeFragment extends BaseFragment {
     private ImageView head2_rec = null;
     private ImageView head2_add = null;
 
-
     private RelativeLayout tool1_Scan;
     private RelativeLayout tool1_Pay;
     private RelativeLayout tool1_Rec;
@@ -46,7 +48,24 @@ public class HomeFragment extends BaseFragment {
 
     private RelativeLayout tool2View;
 
+
+    //整个头部消息部分,余额,转账功能,助手信息
+    private RelativeLayout homeTitle2BlanceView;
+    private TextView homeTitle2Count;
+    private RelativeLayout homeTransferView;
+    private RelativeLayout homePaybackView;
+    private RelativeLayout homeCardView;
+    private RelativeLayout homeShareView;
+    private RelativeLayout homeAssistantView;
+    private TextView homeTitle2AssistantMsg1;
+    private TextView homeTitle2AssistantMsg2;
+    private TextView homeTitle2AssistantMsg1Time;
+    private TextView homeTitle2AssistantMsg2Time;
+
     private RecyclerView myRecyclerView;
+
+
+    private ArrayList<Object> datas;
 
     @Override
     protected void initListener() {
@@ -64,23 +83,41 @@ public class HomeFragment extends BaseFragment {
 
         myRecyclerView = (RecyclerView) rootView.findViewById(R.id.home_fragment_recycleview);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
+        datas = new ArrayList<>();
+        myRecyclerView.setAdapter(new HomeRecycleviewAdapter(getActivity(), datas));
         collapsingToolbarLayout = (CollapsingToolbarLayout) rootView.findViewById(R.id.home_toolbar_layout);
         collapsingToolbarLayout.setStatusBarScrimColor(getResources().getColor(R.color.home_title1_color));
-        mAppBarLayout=(AppBarLayout)rootView.findViewById(R.id.home_fragment_appbar);
+        mAppBarLayout = (AppBarLayout) rootView.findViewById(R.id.home_fragment_appbar);
 
         mToolbar1 = (View) rootView.findViewById(R.id.home_fragment_toolbar1);
         mToolbar2 = (View) rootView.findViewById(R.id.home_fragment_toolbar2);
 
-        head1_srarchimg = (ImageView) rootView.findViewById(R.id.toolbar_head1_serachicon);
-        head1_Search_Et = (EditText) rootView.findViewById(R.id.toolbar_head1_serach);
-        head1_Contract = (ImageView) rootView.findViewById(R.id.toolbar_head1_contract);
-        head1_add = (ImageView) rootView.findViewById(R.id.toolbar_head1_add);
+        head1_srarchimg = (ImageView) rootView.findViewById(R.id.toolbar_head1_serachicon);//展开 搜索图表
+        head1_Search_Et = (EditText) rootView.findViewById(R.id.toolbar_head1_serach);//展开 输入框
+        head1_Contract = (ImageView) rootView.findViewById(R.id.toolbar_head1_contract);//展开 联系人
+        head1_add = (ImageView) rootView.findViewById(R.id.toolbar_head1_add); //展开 加号
 
-        head2_scan = (ImageView) rootView.findViewById(R.id.toolbar_head2_scan);
-        head2_Pay = (ImageView) rootView.findViewById(R.id.toolbar_head2_pay);
-        head2_rec = (ImageView) rootView.findViewById(R.id.toolbar_head2_receive);
-        head2_add = (ImageView) rootView.findViewById(R.id.toolbar_head2_add);
+        head2_scan = (ImageView) rootView.findViewById(R.id.toolbar_head2_scan);//收起 扫码
+        head2_Pay = (ImageView) rootView.findViewById(R.id.toolbar_head2_pay);//收起 付款
+        head2_rec = (ImageView) rootView.findViewById(R.id.toolbar_head2_receive);//收起 收到
+        head2_add = (ImageView) rootView.findViewById(R.id.toolbar_head2_add);//收起 +
+
+
+        homeTitle2BlanceView = (RelativeLayout) rootView.findViewById(R.id.home_title2_blance_view);//账户余额view
+        homeTitle2Count = (TextView) rootView.findViewById(R.id.home_title2_count);//账户余额
+        homeTransferView = (RelativeLayout) rootView.findViewById(R.id.home_transfer_view);//转账
+        homePaybackView = (RelativeLayout) rootView.findViewById(R.id.home_payback_view);//还款
+        homeCardView = (RelativeLayout) rootView.findViewById(R.id.home_card_view);//卡券
+        homeShareView = (RelativeLayout) rootView.findViewById(R.id.home_share_view);//分享
+        homeAssistantView = (RelativeLayout) rootView.findViewById(R.id.home_assistant_view); //助手view
+        homeTitle2AssistantMsg1 = (TextView) rootView.findViewById(R.id.home_title2_assistant_msg1); //助手信息1
+        homeTitle2AssistantMsg2 = (TextView) rootView.findViewById(R.id.home_title2_assistant_msg2);//信息2
+        homeTitle2AssistantMsg1Time = (TextView) rootView.findViewById(R.id.home_title2_assistant_msg1_time);//信息1时间
+        homeTitle2AssistantMsg2Time = (TextView) rootView.findViewById(R.id.home_title2_assistant_msg2_time);//信息2时间
+
+        //整个新闻+资讯打算做成一个统一的recycleview,以数据改变为分界线。
+        //大概为 datas 中数据对象Data为  { news{},info{}} ,=》或者直接合并为,反正数据都差不多 {news{}}
+
 
 
         mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
